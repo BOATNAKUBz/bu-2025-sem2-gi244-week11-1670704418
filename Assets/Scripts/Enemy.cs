2b36b0558caca292e0d337d4d6e8644b659e84c3
BOATNAKUBz <Natthaphon.sueb@bumail.net>
2026-03-27 17:03:25 +0700

    final

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -6,14 +7,20 @@ public class Enemy : MonoBehaviour
     private Rigidbody rb;
     private GameObject player;
 
+
+    
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 dir = player.transform.position - transform.position;
+        dir.Normalize();
+        rb.AddForce(dir * speed);
     }
+
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -5,9 +6,71 @@ public class SpawnManager : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject enemyPrefab;
 
+    //public Rigidbody box;
+
+
     void Start()
     {
+        StartCoroutine(Spawnroutine());
+        //InvokeRepeating(nameof (RandomSpaawm),0 , 5 );
 
+        //StartCoroutine(Hello());
+
+        //StartCoroutine(MoveBox());
+
+        //goodByeRoutine = StartCoroutine(Goodbye());
     }
 
+    IEnumerator Spawnroutine()
+    {
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+
+        void RandomSpawn()
+        {
+            var index = Random.Range(0, spawnPoints.Length);
+            var spawnPoin = spawnPoints[index];
+            Instantiate(enemyPrefab, spawnPoin);
+        }
+
+        // private void Update()
+        //  {
+        // if (Time.time > 5)
+        // {
+        //     StopCoroutine(goodByeRoutine);s
+
+        //   }
+        // }
+        // IEnumerator Goodbye()
+        // {
+        //   while (true)
+        //   {
+        // Debug.Log("GoodBye" + Time.frameCount + " " + Time.time);
+        //       yield return null;
+
+        //}
+        //}
+        /*IEnumerator MoveBox()
+        {
+            box.linearVelocity = 10 * Vector3.up;
+            yield return null; new WaitForSeconds(3);
+            box.linearVelocity = 10 * Vector3.right;
+            yield return null; new WaitForSeconds(3);
+            box.linearVelocity = 10 * Vector3.down;
+            yield return null; new WaitForSeconds(3);
+            box.linearVelocity = 10 * Vector3.left;
+            yield return null; new WaitForSeconds(3);
+
+        }
+
+
+        IEnumerator Hello()
+        {
+            Debug.Log("Hello" + Time.frameCount);
+            yield return null;
+        }*/
+    }
 }
